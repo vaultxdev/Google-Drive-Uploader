2d762cbb889e2399a6b7cb1cdbdf9bfa43c92f62
vaultxdev <69190060+vaultxdev@users.noreply.github.com>
2020-08-07 13:11:20 +0000

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = ""
+    TG_TOKEN = "1048279781:AAEdsnNvv-EZd8sFcoo051tX85-Dmoc3qdE"
     
     
     
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1VHwLvewAtc6hTZb7W9dLDE0g91Oydpwk"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0AGGbxgqLbmlXUk9PVA"
     
     
     
